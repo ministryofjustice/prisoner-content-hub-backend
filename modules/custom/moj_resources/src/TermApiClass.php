77fad84d862fa35a479b2e58f8f8250b79705fb1
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-11-29 07:25:36 +0000

    Add Refactored Term Endpoint with filtering

--- modules/custom/moj_resources/src/TermApiClass.php
+++ modules/custom/moj_resources/src/TermApiClass.php
@@ -3,10 +3,12 @@
 namespace Drupal\moj_resources;
 
 use Drupal\node\NodeInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\Entity\EntityTypeManager;
-use Drupal\Core\Entity\Query\QueryFactory;
-use Symfony\Component\Serializer\Serializer;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * PromotedContentApiClass
@@ -15,38 +17,11 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 class TermApiClass
 {
   /**
-   * Term
-   *
-   * @var array
-   */
-  protected $term;
-  /**
-   * Language Tag
-   *
-   * @var string
-   */
-  protected $lang;
-  /**
-   * Node_storage object
+   * Term storage object
    *
    * @var Drupal\Core\Entity\EntityTypeManager
    */
-  protected $node_storage;
-  /**
-   * Entitity Query object
-   *
-   * @var Drupal\Core\Entity\Query\QueryFactory
-   *
-   * Instance of querfactory
-   */
-  protected $entity_query;
-
-  /**
-   * The custom serializer for terms.
-   *
-   * @var \Symfony\Component\Serializer\Serializer
-   */
-  protected $termSerializer;
+  protected $termStorage;
 
   /**
    * Class Constructor
@@ -55,58 +30,147 @@ class TermApiClass
    * @param QueryFactory $entityQuery
    */
   public function __construct(
-    EntityTypeManagerInterface $entityTypeManager,
-    QueryFactory $entityQuery,
-    Serializer $termSerializer
+    EntityTypeManagerInterface $entityTypeManager
   ) {
-    $this->term_storage = $entityTypeManager->getStorage('taxonomy_term');
-    $this->entity_query = $entityQuery;
-    $this->termSerializer = $termSerializer;
+    $this->termStorage = $entityTypeManager->getStorage('taxonomy_term');
   }
   /**
    * API resource function
    *
-   * @param [string] $lang
-   * @param [string] $category
+   * @param int $termId
    * @return array
    */
-  public function TermApiEndpoint($lang, $term_id)
+  public function TermApiEndpoint($termId, $prisonId)
   {
-    $this->lang = $lang;
-    $terms = $this->term_storage->load($term_id);
-    return $this->decorateResponse($terms);
+    $term = $this->getTerm($termId, $prisonId);
+
+    return $this->createReturnObject($term);
   }
+
+
+  /**
+   * Get Term and filter by Prison/Prison Categories
+   *
+   * @param int $termId
+   * @param int $prisonId
+   *
+   * @return EntityInterface
+  */
+  private function getTerm($termId, $prisonId) {
+    $prison = $this->getPrison($prisonId);
+    $term = $this->termStorage->load($termId);
+
+    if (is_null($term)) {
+      throw new NotFoundHttpException(
+        'Term not found',
+        null,
+        404
+      );
+    }
+
+    if ($term->hasField('field_promoted_to_prison') && !$term->get('field_promoted_to_prison')->isEmpty()) {
+      $selectedPrisonDoesNotMatchRequest = intval($term->get('field_promoted_to_prison')->target_id) !== intval($prisonId);
+
+      if ($selectedPrisonDoesNotMatchRequest) {
+        throw new BadRequestHttpException(
+          "The prison for the term does not match the supplied prison",
+          null,
+          400
+        );
+      }
+
+      return $term;
+    }
+
+    if ($term->hasField('field_prison_categories')
+      && !$term->get('field_prison_categories')->isEmpty()) {
+      $prisonCategories = [];
+
+      foreach($prison->get('field_prison_categories') as $prison_category) {
+        array_push($prisonCategories, intval($prison_category->target_id));
+      }
+
+      $termPrisonCategories = [];
+
+      foreach($term->get('field_prison_categories') as $prison_category) {
+        array_push($termPrisonCategories, intval($prison_category->target_id));
+      }
+
+      $matchingPrisonCategories = array_intersect($prisonCategories, $termPrisonCategories);
+      $hasNoMatchingPrisonCategories = empty($matchingPrisonCategories);
+
+      if ($hasNoMatchingPrisonCategories) {
+        throw new BadRequestHttpException(
+          'The Term does not have a matching prison category for this prison',
+          null,
+          400
+        );
+      }
+    }
+
+    return $term;
+  }
+
+  /**
+   * Get Prison by a Prison ID
+   *
+   * @param int $prisonId
+   *
+   * @return EntityInterface
+  */
+  private function getPrison($prisonId) {
+    $prison = $this->termStorage->load($prisonId);
+
+    if(is_null($prison)) {
+      throw new BadRequestHttpException(
+        'Prison does not exist',
+        null,
+        400
+      );
+    }
+
+    return $prison;
+  }
+
   /**
    * Decorate term response
    *
    * @param Node $term
    * @return array
    */
-  private function decorateResponse($term)
+  private function createReturnObject($term)
   {
-    $result = [];
-    $result['id'] = $term->tid->value;
-    $result['content_type'] = $term->vid[0]->target_id;
-    $result['title'] = $term->name->value;
-    $result['description'] = $term->description[0];
-    $result['summary'] = $term->field_content_summary ? $term->field_content_summary->value : '';
-    $result['image'] = $term->field_featured_image[0];
-    $result['video'] = $term->field_featured_video[0];
-    $result['audio'] = $term->field_featured_audio[0];
-    $result['programme_code'] = $term->field_feature_programme_code ? $term->field_feature_programme_code->value : '';
+    $response = [];
+    $response['id'] = $term->tid->value;
+    $response['content_type'] = $term->vid[0]->target_id;
+    $response['title'] = $term->name->value;
+    $response['description'] = $term->description[0];
+    $response['summary'] = $term->field_content_summary ? $term->field_content_summary->value : '';
+    $response['image'] = $term->field_featured_image[0];
+    $response['video'] = $term->field_featured_video[0];
+    $response['audio'] = $term->field_featured_audio[0];
+    $response['programme_code'] = $term->field_feature_programme_code ? $term->field_feature_programme_code->value : '';
 
-    return $result;
-  }
+    if ($term->hasField('field_prison_categories')) {
+      $prisonCategories = [];
 
-  /**
-   * TranslateNode function
-   *
-   * @param NodeInterface $term
-   *
-   * @return $term
-   */
-  protected function translateNode($term)
-  {
-    return $term->hasTranslation($this->lang) ? $term->getTranslation($this->lang) : $term;
+      foreach($term->get('field_prison_categories') as $prisonCategory) {
+        array_push($prisonCategories, $prisonCategory->target_id);
+      }
+
+      $response['prison_categories'] = $prisonCategories;
+    }
+
+    if ($term->hasField('field_promoted_to_prison')) {
+      $prisons = [];
+
+      foreach($term->get('field_promoted_to_prison') as $prison) {
+        array_push($prisons, $prison->target_id);
+      }
+
+      $response['prisons'] = $prisons;
+    }
+
+    return $response;
   }
 }
