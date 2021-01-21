e54e29cea51762450f99e502f44c6c9a32156fa6
Lucas Cairns <lucas.cairns@gmail.com>
2021-01-21 16:44:27 +0000

    Bugfix/vocabulary endpoint filtering (#152)
    * Update vocabulary prison category filter to only apply for series taxonomy
    
    * Add prison category filtering when validating Secondary Tags have content

--- modules/custom/moj_resources/src/VocabularyApiClass.php
+++ modules/custom/moj_resources/src/VocabularyApiClass.php
@@ -9,8 +9,6 @@ use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\Query\QueryFactory;
 
-require_once('Utils.php');
-
 /**
  * PromotedContentApiClass
  */
@@ -109,7 +107,16 @@ class VocabularyApiClass
         ->condition('type', $types, 'IN')
         ->accessCheck(false);
 
-      $query = getPrisonResults($this->prisonId, $query);
+      $prison = Utilities::getTermFor($this->prisonId, $this->termStorage);
+      $prisonCategories = Utilities::getPrisonCategoriesFor($prison);
+
+      $prisonCategoriesCondition = Utilities::filterByPrisonCategories(
+        $this->prisonId,
+        $prisonCategories,
+        $query
+      );
+
+      $query->condition($prisonCategoriesCondition);
 
       $group = $query
         ->orConditionGroup()
@@ -141,10 +148,14 @@ class VocabularyApiClass
      */
     protected function getVocabularyTermIds($taxonomyName)
     {
+
+      $query = $this->entityQuery->get('taxonomy_term');
+      $query->condition('vid', $taxonomyName);
+
+      if ($taxonomyName == "series") {
+
         $prison = Utilities::getTermFor($this->prisonId, $this->termStorage);
         $prisonCategories = Utilities::getPrisonCategoriesFor($prison);
-
-        $query = $this->entityQuery->get('taxonomy_term');
 
         $prisonCategoriesCondition = Utilities::filterByPrisonCategories(
           $this->prisonId,
@@ -153,10 +164,10 @@ class VocabularyApiClass
           true
         );
 
-        $query
-            ->condition('vid', $taxonomyName)
-            ->condition($prisonCategoriesCondition);
+        $query->condition($prisonCategoriesCondition);
 
-        return $query->execute();
+      }
+
+      return $query->execute();
     }
 }
