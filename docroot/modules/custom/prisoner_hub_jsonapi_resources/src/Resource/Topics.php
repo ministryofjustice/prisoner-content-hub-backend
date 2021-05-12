303071b015e68cf02078b3972c76f797ddef6840
Leon Kessler <leon.kessler@digital.justice.gov.uk>
2021-05-12 15:34:25 +0100

    Adding custom jsonapi resource for the topics page.

---
+++ docroot/modules/custom/prisoner_hub_jsonapi_resources/src/Resource/Topics.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\prisoner_hub_jsonapi_resources\Resource;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\jsonapi\ResourceResponse;
+use Drupal\jsonapi_resources\Resource\EntityQueryResourceBase;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Processes a request for a collection of "topics".
+ *
+ * Topics (currently) refers to categories and tags Taxonomy terms.
+ *
+ * @internal
+ */
+class Topics extends EntityQueryResourceBase {
+
+  /**
+   * Process the resource request.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   *
+   * @return \Drupal\jsonapi\ResourceResponse
+   *   The response.
+   */
+  public function process(Request $request): ResourceResponse {
+    $cacheability = new CacheableMetadata();
+    $entity_query = $this->getEntityQuery('taxonomy_term')
+      ->condition('vid', ['moj_categories', 'tags'], 'IN');
+    $cacheability->addCacheContexts(['url.path']);
+    $cacheability->addCacheTags(['taxonomy_term_list']);
+
+    $data = $this->loadResourceObjectDataFromEntityQuery($entity_query, $cacheability);
+
+    $response = $this->createJsonapiResponse($data, $request, 200, []);
+    $response->addCacheableDependency($cacheability);
+
+    return $response;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRouteResourceTypes($route, string $route_name): array {
+    return $this->getResourceTypesByEntityTypeId('taxonomy_term');
+  }
+}
