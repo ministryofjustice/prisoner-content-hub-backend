06daba548385fcd539af324aa0153ad40e057746
Leon Kessler <leon.kessler@digital.justice.gov.uk>
2021-08-06 16:43:16 +0100

    Modify decoupled router paths, to use prison context.

--- docroot/modules/custom/prisoner_hub_prison_context/src/Routing/RouteSubscriber.php
+++ docroot/modules/custom/prisoner_hub_prison_context/src/Routing/RouteSubscriber.php
@@ -4,6 +4,7 @@ namespace Drupal\prisoner_hub_prison_context\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Drupal\jsonapi\Routing\Routes;
+use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
@@ -18,6 +19,12 @@ class RouteSubscriber extends RouteSubscriberBase {
    */
   protected $jsonapiBasePath;
 
+  /**
+   * RouteSubscriber constructor.
+   *
+   * @param string $jsonapi_base_path
+   *   The jsonapi base path parameter.
+   */
   public function __construct(string $jsonapi_base_path) {
     $this->jsonapiBasePath = $jsonapi_base_path;
   }
@@ -30,14 +37,48 @@ class RouteSubscriber extends RouteSubscriberBase {
   public function alterRoutes(RouteCollection $collection) {
     foreach ($collection as $name => $route) {
       /* @var \Symfony\Component\Routing\Route $route */
-      if (Routes::isJsonApiRequest($route->getDefaults()) || !empty($route->getDefault('_jsonapi_resource'))) {
+      if (self::isJsonApiRoute($route)) {
         $new_route = clone($route);
-        $new_route->setPath(str_replace($this->jsonapiBasePath, $this->jsonapiBasePath . '/prison/{prison}', $route->getPath()));
-        $parameters = $route->getOption('parameters');
-        $parameters['prison'] = ['type' => 'prison_context'];
-        $new_route->setOption('parameters', $parameters);
+        $this->addPrisonContextToRoute($route, str_replace($this->jsonapiBasePath, $this->jsonapiBasePath . '/prison/{prison}', $route->getPath()));
+        $collection->add('prisoner_hub_prison_context.' . $name, $new_route);
+      }
+      elseif ($name == 'decoupled_router.path_translation') {
+        $new_route = clone($route);
+        $this->addPrisonContextToRoute($route, 'router/prison/{prison}/translate-path');
         $collection->add('prisoner_hub_prison_context.' . $name, $new_route);
       }
     }
+  }
+
+  /**
+   * Add prison context to a route.
+   *
+   * @param \Symfony\Component\Routing\Route $route
+   *   The route to be modified.
+   * @param string $new_path
+   *   The new path for the route.
+   *
+   * @return \Symfony\Component\Routing\Route
+   *   The modified route, with prison context.
+   */
+  protected function addPrisonContextToRoute(Route $route, string $new_path) {
+    $route->setPath($new_path);
+    $parameters = $route->getOption('parameters');
+    $parameters['prison'] = ['type' => 'prison_context'];
+    $route->setOption('parameters', $parameters);
+    return $route;
+  }
+
+  /**
+   * Check to see if a route is a JSON:API request, that should be copied.
+   *
+   * @param $route
+   *   The route to check.
+   *
+   * @return bool
+   *   TRUE if the route is for JsonAPI, FALSE if otherwise.
+   */
+  static public function isJsonApiRoute($route) {
+    return Routes::isJsonApiRequest($route->getDefaults()) || !empty($route->getDefault('_jsonapi_resource'));
   }
 }
