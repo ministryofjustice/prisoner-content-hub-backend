1778699f185c309631193366a5494e634f7e7f46
Leon Kessler <leon.kessler@digital.justice.gov.uk>
2021-08-11 10:00:32 +0100

    Finishing work on entity query access, and updating documentation.

--- docroot/modules/custom/prisoner_hub_entity_access/README.md
+++ docroot/modules/custom/prisoner_hub_entity_access/README.md
@@ -16,17 +16,25 @@ which are checked to see if they match the current prison (which is set from the
 Drupal offers several API's for hiding/restricting content.
 Below are the ones used by this module.
 - hook_entity_access
+  See https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Entity%21entity.api.php/function/hook_entity_access/9.x
   Implemented in `src/EntityAccessCheck.php`
-  This hook only works for when an entity is fully loaded, and this (normally) only happens when viewing the entity directly.
-  I.e. for requests such as `/jsonapi/node/page/{{uuid}}`
-- Entity query access api
+  This hook only works for when an entity is fully loaded, so should be considered as the "last-resort" for hiding/showing
+  content.
+- Entity query access API
   Implemented in `src/EventSubscriber/QueryAccessSubscriber.php`
   This API affects all entity queries, which includes any JSON:API response for a list of content.
-  I.e. for requests such as `/jsonapi/node/page`
+  E.g. for requests such as `/jsonapi/node/page`.
+  Note that without this hook, the results of hook_entity_access will still be respected.  However, because hook_entity_access
+  only works _after_ the entity has been fully loaded, it means that they will be processed _after_ the result count.
+  I.e. if you were requesting 50 nodes, and 5 of them were restricted, you would end up with a JSON:API response of 45
+  nodes.  By using the entity query access API we can be sure that the results will always contain 50 valid nodes.
 - hook_search_api_query_alter
   Implemented in `src/SearchApiQueryAlter.php`
-  This hook works for search_api queries.
-  I.e. for requests such as `/jsonapi/index/{{name_of_index}}`
+  This hook works for search_api queries
+  E.g. for requests such as `/jsonapi/index/{{name_of_index}}`
+  We use this for the same reason as the entity query access api, to ensure
+  our result sets always contain a full set of valid rows.
+
 
 ## Relevant Trello card
 https://trello.com/c/JxFpJDkr/1944-determine-and-implement-method-for-passing-the-current-prison-to-drupal
--- docroot/modules/custom/prisoner_hub_entity_access/src/EntityAccessCheck.php
+++ docroot/modules/custom/prisoner_hub_entity_access/src/EntityAccessCheck.php
@@ -1,16 +1,13 @@
 <?php
 
 namespace Drupal\prisoner_hub_entity_access;
+
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Entity\Query\QueryInterface;
-use Drupal\Core\Render\RenderContext;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\jsonapi\Controller\EntityResource;
-use Drupal\node\NodeAccessControlHandler;
-use LogicException;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\TypedData\TypedDataInterface;
 
 /**
  * Drupal service to be used in conjunction with hook_entity_access().
@@ -18,7 +15,7 @@ use LogicException;
  * Note that hook_entity_access() only works on fully loaded entities, and not
  * when querying for a list of entities (e.g. via an entity query).
  */
-class EntityAccessCheck extends NodeAccessControlHandler {
+class EntityAccessCheck {
 
   /**
    * The prison category loader service.
@@ -33,111 +30,67 @@ class EntityAccessCheck extends NodeAccessControlHandler {
   protected $entityTypeManager;
 
   /**
-   * SearchApiQueryAlter constructor.
+   * EntityAccessCheck constructor.
    *
    * @param \Drupal\prisoner_hub_entity_access\PrisonCategoryLoader $prison_category_loader
-   *   The PrisonCategoryLoader service.
+   * @param \Drupal\Core\Entity\EntityTypeManager $entity_type_manager
    */
-  //  public function __construct(EntityTypeManager $entity_type_manager) {
-  //    $this->entityTypeManager = $entity_type_manager;
-  //  }
-
-  public function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
-    $access = parent::checkAccess($entity, $operation, $account);
-    if ($access->isForbidden()) {
-      return $access;
-    }
-
-    $entity_type = $entity->getEntityType();
-
-
-
-    $cacheability = new CacheableMetadata();
-    $query = \Drupal::entityTypeManager()->getStorage($entity->getEntityTypeId())->getQuery()->condition($entity_type->getKey('id'), $entity->id());
-    $entity_ids = $this->executeQueryInRenderContext($query, $cacheability);
-    $stop = 1;
-    if (empty($entity_ids)) {
-      return AccessResult::forbidden();
-    }
-    return AccessResult::allowed();
+  public function __construct(PrisonCategoryLoader $prison_category_loader, EntityTypeManager $entity_type_manager) {
+    $this->prisonCategoryLoader = $prison_category_loader;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
-   * Executes the query in a render context, to catch bubbled cacheability.
+   * Check access of an entity, based on prison and prison category fields.
    *
-   * @param \Drupal\Core\Entity\Query\QueryInterface $query
-   *   The query to execute to get the return results.
-   * @param \Drupal\Core\Cache\CacheableMetadata $query_cacheability
-   *   The value object to carry the query cacheability.
-   *
-   * @return int|array
-   *   Returns an integer for count queries or an array of IDs. The values of
-   *   the array are always entity IDs. The keys will be revision IDs if the
-   *   entity supports revision and entity IDs if not.
-   *
-   * @see node_query_node_access_alter()
-   * @see https://www.drupal.org/project/drupal/issues/2557815
-   * @see https://www.drupal.org/project/drupal/issues/2794385
-   * @todo Remove this after https://www.drupal.org/project/drupal/issues/3028976 is fixed.
-   */
-  protected function executeQueryInRenderContext(QueryInterface $query, CacheableMetadata $query_cacheability) {
-    return $this->executeInRenderContext(function () use ($query) {
-      return $query->execute();
-    }, $query_cacheability);
-  }
-
-  /**
-   * Executes a callable in a render context.
-   *
-   * @param callable $f
-   *   The function to execute in a render context.
-   * @param \Drupal\Core\Cache\CacheableMetadata $cacheability
-   *   The value object to carry the captured cacheability.
-   *
-   * @return mixed
-   *   Returns the return value of the executed callable.
-   *
-   * @see node_query_node_access_alter()
-   * @see https://www.drupal.org/project/drupal/issues/2557815
-   * @see https://www.drupal.org/project/drupal/issues/2794385
-   * @todo Remove this after https://www.drupal.org/project/drupal/issues/3028976 is fixed.
-   */
-  protected function executeInRenderContext(callable $f, CacheableMetadata $cacheability) {
-    $context = new RenderContext();
-    $ret = \Drupal::service('renderer')->executeInRenderContext($context, function () use ($f) {
-      return $f();
-    });
-    if (!$context->isEmpty()) {
-      $cacheability->addCacheableDependency($context->pop());
-    }
-    return $ret;
-  }
-
-  /**
-   * Checks an entity and returns the appropriate AccessResult().
+   * This method should be called from hook_entity_access().
    *
    * @param \Drupal\Core\Entity\EntityInterface $entity
-   *   The entity object, note that only content entities are checked.
+   *   The entity object, note that only content entities will be handled.
    *
-   * @return \Drupal\Core\Access\AccessResultReasonInterface
-   *   The AccessResult, depending on the prison category filtering rule.
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The AccessResult, determined by the entities prison and prison category
+   *   fields
    */
-  public function getAccessBasedOnPrisonCategory(EntityInterface $entity) {
-    // Only deal with content entities.
-    if (!$entity instanceof ContentEntityInterface) {
+  public function checkAccess(EntityInterface $entity) {
+    $current_prison_id = $this->prisonCategoryLoader->getPrisonIdFromCurrentRoute();
+
+    // Only handle requests where there is a prison in the url.
+    if (!$current_prison_id) {
       return AccessResult::neutral();
     }
-    $entity_type = $entity->getEntityType();
-    try {
-      $entity_ids = $this->entityTypeManager->getStorage($entity->getEntityTypeId())->getQuery()->condition($entity_type->getKey('id'), $entity->id())->execute();
-      if (empty($entity_ids)) {
+
+    if ($entity instanceof ContentEntityInterface) {
+      $prison_field_name = $this->prisonCategoryLoader->getPrisonFieldName();
+      $prison_category_field_name = $this->prisonCategoryLoader->getPrisonCategoryFieldName();
+      if ($entity->hasField($prison_field_name) && $entity->hasField($prison_category_field_name)) {
+        if ($this->fieldValueExists($entity->get($prison_field_name), $current_prison_id)) {
+          return AccessResult::allowed();
+        }
+        if ($this->fieldValueExists($entity->get($prison_category_field_name), $this->prisonCategoryLoader->getPrisonCategoryIdFromCurrentRoute())) {
+          return AccessResult::allowed();
+        }
         return AccessResult::forbidden();
       }
     }
-    catch (LogicException $e) {
+    return AccessResult::neutral();
+  }
 
-    }
-
-    return AccessResult::allowed();
+  /**
+   * Check whether multivalue field contains a specific value.
+   *
+   * @param \Drupal\Core\Field\FieldItemListInterface $field
+   *   The field to check for the existence of a value.
+   * @param $value
+   *   The value to check.
+   *
+   * @return bool
+   *   TRUE if $field contains $value, otherwise FALSE.
+   */
+  protected function fieldValueExists(FieldItemListInterface $field, $value) {
+    $field->filter(function (TypedDataInterface $item) use ($value) {
+      return $item->target_id == $value;
+    });
+    return $field->count() > 0;
   }
 }
