729a6ad48265b982f28ff5b8d7b1ebcdbf0feb0b
Leon Kessler <leon.kessler@digital.justice.gov.uk>
2021-08-26 14:39:39 +0100

    Allow for empty prison ID, to bypass prison check.

--- docroot/modules/custom/moj_resources/src/Utils.php
+++ docroot/modules/custom/moj_resources/src/Utils.php
@@ -1,12 +1,14 @@
 <?php
 function getPrisonResults($prison_id, $results) {
+  if ($prison_id > 0) {
+    $prison_results = $results
+      ->orConditionGroup()
+      ->condition('field_moj_prisons', $prison_id, '=')
+      ->condition('field_moj_prisons', '', '=')
+      ->notExists('field_moj_prisons');
+    $results->condition($prison_results);
+  }
 
-  $prison_results = $results
-    ->orConditionGroup()
-    ->condition('field_moj_prisons', $prison_id, '=')
-    ->condition('field_moj_prisons', '', '=')
-    ->notExists('field_moj_prisons');
-  $results->condition($prison_results);
 
   return $results;
 }
