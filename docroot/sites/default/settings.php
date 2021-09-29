138b4e3ec8e5a2aedca658b1717b312cb2233eea
Leon Kessler <leon.kessler@digital.justice.gov.uk>
2021-09-29 15:00:10 +0100

    Adding fix for Redis causing error during site installation.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -1,5 +1,7 @@
 <?php
 
+use Drupal\Core\Installer\InstallerKernel;
+
 $databases = [];
 $databases['default']['default'] = array(
   'database' => getenv('HUB_DB_ENV_MYSQL_DATABASE', true),
@@ -122,12 +124,21 @@ $config['raven.settings'] = [
   'fatal_error_handler' => true
 ];
 
-$settings['redis.connection']['interface'] = 'Predis';
-$settings['redis.connection']['host'] = getenv('REDIS_HOST', true);
-if (getenv('REDIS_PASSWORD', true)) {
-  $settings['redis.connection']['password'] = getenv('REDIS_PASSWORD', true);
+// Do not load Redis during installation (required for CircleCI builds).
+// See https://www.drupal.org/project/redis/issues/2876132#comment-13054928
+if (!InstallerKernel::installationAttempted()) {
+  $settings['redis.connection']['interface'] = 'Predis';
+  $settings['redis.connection']['host'] = getenv('REDIS_HOST', true);
+  if (getenv('REDIS_PASSWORD', true)) {
+    $settings['redis.connection']['password'] = getenv('REDIS_PASSWORD', true);
+  }
+  $settings['cache']['default'] = 'cache.backend.redis';
+
+  // Load in the services config directly from the module.  This allows
+  // for any updates to be automatically added, and also ensures we do not add
+  // the config during site installation (which will result in an error).
+  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
 }
-$settings['cache']['default'] = 'cache.backend.redis';
 
 $settings['config_sync_directory'] = '../config/sync';
 
