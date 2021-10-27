bebc3434bd64e07167f953e54a29b204a1a8ac78
Leon Kessler <leon.kessler@digital.justice.gov.uk>
2021-10-27 14:31:40 +0100

    Fix missing function causing errors on test.

--- docroot/modules/custom/prisoner_hub_series_access/tests/src/ExistingSite/PrisonerHubSeriesAccessTest.php
+++ docroot/modules/custom/prisoner_hub_series_access/tests/src/ExistingSite/PrisonerHubSeriesAccessTest.php
@@ -68,4 +68,19 @@ class PrisonerHubSeriesAccessTest extends ExistingSiteBase {
     $response = $this->getJsonApiResponse($url);
     $this->assertSame(200, $response->getStatusCode(), $url->toString() . ' returns a 200 response.');
   }
+
+  /**
+   * Get a response from a JSON:API url.
+   *
+   * @param \Drupal\Core\Url $url
+   *   The url object to use for the JSON:API request.
+   *
+   * @return \Psr\Http\Message\ResponseInterface
+   *   The response object.
+   */
+  function getJsonApiResponse(Url $url) {
+    $request_options = [];
+    $request_options[RequestOptions::HEADERS]['Accept'] = 'application/vnd.api+json';
+    return $this->request('GET', $url, $request_options);
+  }
 }
