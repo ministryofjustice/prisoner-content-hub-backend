028481c2944c110d7f4808509c8bbc0c180bebf6
Leon Kessler <leon.kessler@digital.justice.gov.uk>
2022-03-14 13:27:10 +0000

    Adding support for checking whether there is content in the sub-category/series.

--- docroot/modules/custom/prisoner_hub_taxonomy_child_count/src/Plugin/Field/FieldType/ComputedTaxonomyChildCount.php
+++ docroot/modules/custom/prisoner_hub_taxonomy_child_count/src/Plugin/Field/FieldType/ComputedTaxonomyChildCount.php
@@ -5,6 +5,7 @@ namespace Drupal\prisoner_hub_taxonomy_child_count\Plugin\Field\FieldType;
 use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\TypedData\ComputedItemListTrait;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * Represents the computed child_term_count field.
@@ -21,14 +22,29 @@ class ComputedTaxonomyChildCount extends FieldItemList implements FieldItemListI
       return NULL;
     }
     $result = [];
-    $result['sub_categories_count'] = \Drupal::entityQuery('taxonomy_term')
+
+    $result['sub_categories_count'] = 0;
+    $sub_categories_result = \Drupal::entityQuery('taxonomy_term')
       ->condition('parent', $entity->id())
-      ->count()
       ->execute();
-    $result['sub_series_count'] = \Drupal::entityQuery('taxonomy_term')
+    /** @var \Drupal\taxonomy\TermInterface $term */
+    foreach (Term::loadMultiple($sub_categories_result) as $term) {
+      if ($term->access('view')) {
+        $result['sub_categories_count']++;
+      }
+    }
+
+    $result['sub_series_count'] = 0;
+    $sub_series_result = \Drupal::entityQuery('taxonomy_term')
       ->condition('field_category', $entity->id())
-      ->count()
       ->execute();
+    /** @var \Drupal\taxonomy\TermInterface $term */
+    foreach (Term::loadMultiple($sub_series_result) as $term) {
+      if ($term->access('view')) {
+        $result['sub_series_count']++;
+      }
+    }
+
     $this->list[0] = $this->createItem(0, $result);
   }
 }
