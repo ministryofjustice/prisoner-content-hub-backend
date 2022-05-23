8434db8861927275a16d84e467fcd95a169897ab
Leon Kessler <leon.nk@gmail.com>
2022-05-23 10:39:06 +0100

    Removing typo.

--- docroot/modules/custom/prisoner_hub_prison_access_cms/tests/src/ExistingSite/PrisonerHubPrisonAccessCmsTest.php
+++ docroot/modules/custom/prisoner_hub_prison_access_cms/tests/src/ExistingSite/PrisonerHubPrisonAccessCmsTest.php
@@ -57,7 +57,7 @@ class PrisonerHubPrisonAccessCmsTest extends ExistingSiteBase {
   public function testUserCanCreateNewContent() {
     foreach ($this->contentTypes as $contentType) {
       // Skip the homepage content type, as we do not have access to create these.
-      if (in_array($contentType, ['featured_articles', 'homepage'])config/sync/user.role.local_administrator.yml) {
+      if (in_array($contentType, ['featured_articles', 'homepage'])) {
         continue;
       }
       $this->visit('/node/add/' . $contentType);
