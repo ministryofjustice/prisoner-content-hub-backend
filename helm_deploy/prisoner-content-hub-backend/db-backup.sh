5c56ef2a435262b231d3ba99087660bc96bd5c8a
Leon Kessler <leon.nk@gmail.com>
2022-09-02 09:50:25 +0100

    Adding success message.

--- helm_deploy/prisoner-content-hub-backend/db-backup.sh
+++ helm_deploy/prisoner-content-hub-backend/db-backup.sh
@@ -16,3 +16,5 @@ echo "aws_access_key_id=${DB_BACKUP_S3_KEY}" >> ~/.aws/credentials
 echo "aws_secret_access_key=${DB_BACKUP_S3_SECRET}" >> ~/.aws/credentials
 
 aws s3 mv ~/${filename} s3://${DB_BACKUP_S3_BUCKET}/${filename} --region=${DB_BACKUP_S3_REGION}
+
+echo "Successfuly backed up database ${filename}"
