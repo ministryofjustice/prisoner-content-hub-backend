3d938f5d52345df7f4d73c5c24139eec056caee0
Leon Kessler <leon.nk@gmail.com>
2022-11-21 14:30:35 +0000

    Reduce ttl to 3 months.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -48,7 +48,7 @@ $settings['flysystem'] = [
 
       'options' => [
         'ACL' => 'private',
-        'CacheControl' => 'max-age=290304000, public',
+        'CacheControl' => 'max-age=7890000, public',
       ],
 
       // Directory prefix for all viewed files
