a5af6d3ef5b159d6ed4bb33080e267a8d1d9551f
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2023-08-31 12:03:30 +0100

    Revert "Use service account for AWS IAM creds (#541)"
    This reverts commit 0e87244a2cbffaba63739dc39bfee3d03add301c.

--- helm_deploy/prisoner-content-hub-backend/s3-sync.sh
+++ helm_deploy/prisoner-content-hub-backend/s3-sync.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ue
 
+mkdir ~/.aws/
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id=${S3_DESTINATION_KEY}" >> ~/.aws/credentials
+echo "aws_secret_access_key=${S3_DESTINATION_SECRET}" >> ~/.aws/credentials
+
 aws s3 sync  \
   s3://${S3_SOURCE_BUCKET} \
   s3://${S3_DESTINATION_BUCKET} \
