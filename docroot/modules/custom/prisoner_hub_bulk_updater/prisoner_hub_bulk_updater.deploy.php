0d67b37223bf80a4da10aae088b7713d2303c1ef
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2024-04-03 16:30:48 +0100

    LNP-638: 🫧 initial cleanse mechanism with small amount of test unpublished content.

--- docroot/modules/custom/prisoner_hub_bulk_updater/prisoner_hub_bulk_updater.deploy.php
+++ docroot/modules/custom/prisoner_hub_bulk_updater/prisoner_hub_bulk_updater.deploy.php
@@ -71,3 +71,56 @@ function prisoner_hub_bulk_updater_deploy_woodhill_red_content(array &$sandbox):
     '@nids' => implode('|', $batch_nids),
   ]);
 }
+
+/**
+ * Implements cleanse phase 1.
+ */
+function prisoner_hub_bulk_updater_deploy_cleanse_1() {
+  if (!isset($sandbox['total'])) {
+    $sandbox['current'] = 0;
+
+    $module_path = \Drupal::service('extension.list.module')->getPath('prisoner_hub_bulk_updater');
+    $cleanse_csv_path = "{$module_path}/files/cleanse_1.csv";
+    $cleanse_csv_file = fopen($cleanse_csv_path, 'r');
+
+    $sandbox['nids'] = [];
+    while (($line = fgets($cleanse_csv_file)) !== FALSE) {
+      if (intval($line) != 0) {
+        $sandbox['nids'][] = intval($line);
+      }
+    };
+    fclose($cleanse_csv_file);
+
+    $sandbox['total'] = count($sandbox['nids']);
+  }
+
+  $batch_size = 10;
+  $batch_count = 0;
+  $batch_nids = [];
+  while ($batch_count < $batch_size && $sandbox['current'] < $sandbox['total']) {
+    $node = Node::load($sandbox['nids'][$sandbox['current']]);
+    if ($node) {
+      $node->setUnpublished();
+      try {
+        $node->save();
+        $batch_nids[] = $sandbox['nids'][$sandbox['current']];
+      }
+      catch (Exception $e) {
+        \Drupal::logger('prisoner_hub_bulk_updater')->notice('Exception whilst unpublishing node @nid: @text', [
+          '@nid' => $sandbox['nids'][$sandbox['current']],
+          '@text' => $e->getMessage(),
+        ]);
+      }
+    }
+    $sandbox['current']++;
+    $batch_count++;
+  }
+
+  $sandbox['#finished'] = (float) $sandbox['current'] / $sandbox['total'];
+
+  return t("Updated @count of @total records\nNode IDs: @nids", [
+    '@count' => $sandbox['current'],
+    '@total' => $sandbox['total'],
+    '@nids' => implode('|', $batch_nids),
+  ]);
+}
