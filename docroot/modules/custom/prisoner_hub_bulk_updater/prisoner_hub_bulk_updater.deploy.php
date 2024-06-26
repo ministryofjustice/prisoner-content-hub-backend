0c1ae1991a4de444cce7d9eea71d7f8215c3f95f
Sweety Mariam John <162119856+sweetymj@users.noreply.github.com>
2024-06-26 09:22:04 +0100

    LNP-826: red content cookhamwood (#598)
    * LNP-826-bulk-updates-and-functionality
    
    * LNP-826-sorting-deleting-duplicates-entries

--- docroot/modules/custom/prisoner_hub_bulk_updater/prisoner_hub_bulk_updater.deploy.php
+++ docroot/modules/custom/prisoner_hub_bulk_updater/prisoner_hub_bulk_updater.deploy.php
@@ -75,6 +75,71 @@ function prisoner_hub_bulk_updater_deploy_woodhill_red_content(array &$sandbox):
 }
 
 /**
+ * Excludes specific content from Cookhamwood.
+ *
+ * @param array $sandbox
+ *   Stores information for batch updates.
+ *
+ * @return string
+ *   Message displayed to user after update complete.
+ */
+function prisoner_hub_bulk_updater_deploy_cookhamwood_red_content(array &$sandbox): string {
+  if (!isset($sandbox['total'])) {
+    $sandbox['current'] = 0;
+
+    $module_path = \Drupal::service('extension.list.module')->getPath('prisoner_hub_bulk_updater');
+    $red_csv_path = "{$module_path}/files/cookhamwood_red_nids.csv";
+    $red_csv_file = fopen($red_csv_path, 'r');
+
+    $sandbox['nids'] = [];
+    while (($line = fgets($red_csv_file)) !== FALSE) {
+      if (intval($line) != 0) {
+        $sandbox['nids'][] = intval($line);
+      }
+    };
+    fclose($red_csv_file);
+
+    $sandbox['total'] = count($sandbox['nids']);
+  }
+
+  $batch_size = 10;
+  $batch_count = 0;
+  $batch_nids = [];
+  while ($batch_count < $batch_size && $sandbox['current'] < $sandbox['total']) {
+    $node = Node::load($sandbox['nids'][$sandbox['current']]);
+    if ($node) {
+      $excluded_prisons = $node->get('field_exclude_from_prison')->getValue() ?? [];
+      $excluded_prisons[] = ['target_id' => '959'];
+      $node->set('field_exclude_from_prison', $excluded_prisons);
+      $node->setNewRevision(TRUE);
+      $node->setRevisionCreationTime(\Drupal::time()->getCurrentTime());
+      $node->setRevisionLogMessage('Bulk update to remove content from Cookhamwood');
+      $node->setRevisionUserId(1);
+      try {
+        $node->save();
+        $batch_nids[] = $sandbox['nids'][$sandbox['current']];
+      }
+      catch (Exception $e) {
+        \Drupal::logger('prisoner_hub_bulk_updater')->notice('Exception whilst saving node @nid: @text', [
+          '@nid' => $sandbox['nids'][$sandbox['current']],
+          '@text' => $e->getMessage(),
+        ]);
+      }
+    }
+    $sandbox['current']++;
+    $batch_count++;
+  }
+
+  $sandbox['#finished'] = (float) $sandbox['current'] / $sandbox['total'];
+
+  return t("Updated @count of @total records\nNode IDs: @nids", [
+    '@count' => $sandbox['current'],
+    '@total' => $sandbox['total'],
+    '@nids' => implode('|', $batch_nids),
+  ]);
+}
+
+/**
  * Renames terms.
  *
  * @return \Drupal\Core\StringTranslation\TranslatableMarkup
