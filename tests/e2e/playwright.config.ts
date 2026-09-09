8947717a2c65be66fa728c2c1b2c7a75e88970f8
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2026-09-09 10:35:00 +0100

    LNP-1795: 🔧  move local Drupal port from 11001 to 8080 to help testing cache warming. (#712)

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 5 : 2,
   reporter: [['list'], ['html', { open: 'never' }]],
   use: {
-    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:11001',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:8080',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
